d5d4016b60b84e1045754c03336af846e4748cdf
Franco Bocanegra <franco.bocanegra@outlook.com>
2026-08-04 20:15:14 -0300

    se termino de colocar en forma para la preentrega 3

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import { StyleSheet } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import ProfileScreens from './src/screens/ProfileScreens'
 import { colors } from './src/constants/colors'
+import AddTaskScreen from './src/screens/AddTaskScreen'
+import MenuPrincipal from './src/screens/MenuPrincipal'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <ProfileScreens />
+      <MenuPrincipal />      
     </SafeAreaView>
   )
 }
--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -5,13 +5,17 @@ export const colors = {
   botonColor: '#6354d8',
   footerColor: '#5a50cf',
   //colores dados por ia
-    primary: "#4F46E5",
+  primary: "#4F46E5",
   secondary: "#818CF8",
   background: "#F4F5FA",
   card: "#FFFFFF",
   text: "#1F2937",
   textSecondary: "#6B7280",
   border: "#E5E7EB",
+  //mas colores
+  error: "#DC2626",
+  success: "#16A34A",
+  white: "#FFFFFF",
 }
 
 export default colors;
